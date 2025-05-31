a5af25f66581d4a375385d83917dfc89665116d6
Tereza Lunerova <lunerova.tereza@seznam.cz>
2025-05-31 12:24:15 +0200

    ukol16

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,63 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Joke } from '../../components/joke';
 import './style.css';
 
 export const HomePage = () => {
-  const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState(0);
-  const [jokeText, setJokeText] = useState('Načítám vtip...');
+  const [jokes, setJokes] = useState([]);
 
   useEffect(() => {
-    const fetchJoke = async () => {
+    const fetchJokes = async () => {
       try {
-        const response = await fetch('http://localhost:4000/api/vtipy');
-        const json = await response.json();
-        const first = json.data[0];
-        setJokeText(first.text);
-      } catch (error) {
-        setJokeText('Nepodařilo se načíst vtip 😢');
-        console.error('Chyba při načítání:', error);
+        const res = await fetch('http://localhost:4000/api/vtipy');
+        const json = await res.json();
+        setJokes(json.data);
+      } catch (err) {
+        console.error('Chyba při načítání vtipů:', err);
       }
     };
 
-    fetchJoke();
+    fetchJokes();
   }, []);
 
   return (
     <div className="container">
       <header>
-        <div className="logo"></div>
+        <h1>Dad jokes</h1>
       </header>
       <main>
-        <div className="container">
-          <div className="joke">
-            <div className="joke__body">
-              <div className="joke__user">
-                <img
-                  className="user-avatar"
-                  src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-                  alt="Neroxx"
-                />
-                <p className="user-name">Neroxx</p>
-              </div>
-
-              <p className="joke__text">{jokeText}</p>
-            </div>
-
-            <div className="joke__likes">
-              <button
-                className="btn-like btn-like--up"
-                onClick={() => setLikes(likes + 1)}
-              ></button>
-              <span className="likes-count likes-count--up">{likes}</span>
-
-              <button
-                className="btn-like btn-like--down"
-                onClick={() => setDislikes(dislikes + 1)}
-              ></button>
-              <span className="likes-count likes-count--down">{dislikes}</span>
-            </div>
-          </div>
-        </div>
+        {jokes.map((joke) => (
+          <Joke
+            key={joke.id}
+            userAvatar={joke.avatar}
+            userName={joke.name}
+            text={joke.text}
+            likes={joke.likes}
+            dislikes={joke.dislikes}
+          />
+        ))}
       </main>
       <footer>
         <p>Czechitas, Digitální akademie: Web</p>
